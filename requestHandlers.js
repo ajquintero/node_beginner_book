5a1ad8a3521ec921971efab2e366b0e9cd196c75
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-13 21:33:35 -0800

    passes the POST into the router, data callback is called multiple times

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,18 +1,25 @@
 var exec = require("child_process").exec;
 
+
 function start() {
   console.log("Request handler 'start' was called.");
 
-  function sleep() {
-    var content = "empty";
-    
-    exec("ls -lah", function (error, stdout, stderr) {
-      Response.writeHead(200, {"Content-Type": "text/plain"});
-      Response.write(stdout);
-      Response.end();
-    });
+  var body = `'<html>'+
+  5 '<head>'+
+  6 '<meta http-equiv="Content-Type" content="text/html; '+
+  7 'charset=UTF-8" />'+
+  8 '</head>'+
+  9 '<body>'+
+  10 '<form action="/upload" method="post">'+
+  11 '<textarea name="text" rows="20" cols="60"></textarea>'+
+  12 '<input type="submit" value="Submit text" />'+
+  13 '</form>'+
+  14 newLocal+
+  15 '</html>';`;
 
-  }
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(body);
+  response.end();
 
 }
 
