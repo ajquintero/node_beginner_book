666922aff12480030dddf678d61835824592f8eb
Abel Quintero <abel.j.quintero@gmail.com>
2019-01-13 21:35:34 -0800

    able to receive POST data and use it in our request handlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,7 @@
 var exec = require("child_process").exec;
 
 
-function start() {
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
   var body = `'<html>'+
@@ -24,10 +24,10 @@ function start() {
 }
 
 
-function upload(response) {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent: " +postData);
   response.end;
 }
 
